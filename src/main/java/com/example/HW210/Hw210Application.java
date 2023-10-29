6df21f8298a0a9ef373d7f6deeec70c589553c66
Mick210 <sid222m@gmail.com>
2023-10-29 20:39:17 +0300

    commit

---
+++ src/main/java/com/example/HW210/Hw210Application.java
@@ -0,0 +1,13 @@
+package com.example.HW210;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw210Application.class, args);
+	}
+
+}
